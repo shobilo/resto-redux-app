eb22becf346a3b8369e7db38f6b91e2a6fa51509
shobilo <58330458+shobilo@users.noreply.github.com>
2021-08-28 22:06:54 +0300

    redux architecture was created

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,18 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import MenuListItem from '../menu-list-item';
 
 import './menu-list.scss';
 
-class MenuList extends Component {
-
-    render() {
-
-        return (
-            <ul className="menu__list">
-                <MenuListItem/>
-            </ul>
-        )
-    }
+const MenuList = () => {
+    return (
+        <ul className="menu__list">
+            <MenuListItem/>
+        </ul>
+    )
 };
 
 
