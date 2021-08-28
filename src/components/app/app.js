d2c5bc453c42238842b27e0cfc8d71572607e701
shobilo <58330458+shobilo@users.noreply.github.com>
2021-08-28 22:36:02 +0300

    primitive routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,16 +2,22 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc/with-resto-service';
+import { Switch, Route } from 'react-router';
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
-    console.log(RestoService.getMenuItems())
+    
+    RestoService.getMenuItems()
+        .then( menu => console.log(menu))
+        .catch(error => {console.log('Произошла ошибка')})
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                <Route path="/" exact component={MainPage}/>
+                <Route path="/cart" exact component={CartPage}/>
+            </Switch>
         </div>
     )
 }
